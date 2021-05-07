c37ee022b0b793d0ec5e455666693e83879cfeb0
rek655869 <e.gladneva1357@gmail.com>
2021-05-07 14:40:53 +0300

    v 1.0.14 bug fix

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import yaml
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import handler
 
+
 with open('settings.yaml', encoding='utf8') as f:
     settings = yaml.safe_load(f)
 token, confirmation_token, group_id = settings['token'], settings['confirmation_token'], settings['group_id']
 
-while True:
-    vk_session = vk_api.VkApi(token=token)
-    longpoll = VkBotLongPoll(vk_session, group_id, wait=25)
-    vk = vk_session.get_api()
-    LongPollServer = vk.groups.getLongPollServer(group_id=group_id)
-    key, server, ts = LongPollServer['key'], LongPollServer['server'], LongPollServer['ts']
-    config = {'key': key, 'server': server, 'ts': ts}
+vk_session = vk_api.VkApi(token=token)
+longpoll = VkBotLongPoll(vk_session, group_id, wait=25)
+vk = vk_session.get_api()
+LongPollServer = vk.groups.getLongPollServer(group_id=group_id)
+key, server, ts = LongPollServer['key'], LongPollServer['server'], LongPollServer['ts']
+config = {'key': key, 'server': server, 'ts': ts}
 
+while True:
     try:
         for event in longpoll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW and event.from_user:
@@ -23,4 +24,10 @@ while True:
     except Exception as e:
         with open('errors.txt', 'a') as f:
             f.write(str(e) + '\n')
+        vk_session = vk_api.VkApi(token=token)
+        longpoll = VkBotLongPoll(vk_session, group_id, wait=25)
+        vk = vk_session.get_api()
+        LongPollServer = vk.groups.getLongPollServer(group_id=group_id)
+        key, server, ts = LongPollServer['key'], LongPollServer['server'], LongPollServer['ts']
+        config = {'key': key, 'server': server, 'ts': ts}
         continue
