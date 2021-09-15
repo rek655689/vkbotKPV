12a98c87d6093e3cfe632c35d3d0d667899995e0
Rek <80971380+rek655689@users.noreply.github.com>
2021-09-15 15:32:09 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,9 +0,0 @@
-token = '166fc649621287dbc237506598351782c2e8f04f720db82dcbe3e5ac0ae4704c5a77b1e36d797afd9f275'
-confirmation_token = '75889628'
-group_id = 169574362
-access_token = '6d874a8c57f45642c116ff55761a016a0cfcfaed059c9fa309bd271d21ed9853a173c6c7cac03791fae03'
-editor = 478936081
-
-database = {'host': 'localhost', 'user': 'root', 'password': 'mtucfDfjBn@82sQ'}
-
-catwar = {'mail': 'rek655869@gmail.com', 'pass': 'n73SGfnvyTZQBWF', 'cat': 'Вздох Восхищения'}
