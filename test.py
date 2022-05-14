51d6b5d350bfdaf765dea08943d36a9e02f3db05
rek655689 <e.gladneva1357@gmail.com>
2022-05-14 12:00:07 +0300

    upd

--- test.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-import calendar
-now = datetime.now()
-month = calendar.monthcalendar(now.year, now.month)
-sundays = [week[0] for week in month if week[0]>0]
-a = sundays[1]
-print(a == now.day)
